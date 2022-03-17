2ccf97ffb94ea7edc576f8550f7e79eadc3315cc
BENNY881019 <81384262+BENNY881019@users.noreply.github.com>
2022-03-18 00:19:20 +0800

    Update webp.c

--- 新增資料夾/webp.c
+++ 新增資料夾/webp.c
@@ -4,7 +4,7 @@ int main(int argc, char **argv){
 	MyImage *img;
 	
 	img = createMyImage(128, 128);
-	fillcolor(img,255, 128, 0);
+	fillColor(img,255, 0, 0);
 	saveImage(img, "test.webp");
 	freeMyImage(img);
 	
